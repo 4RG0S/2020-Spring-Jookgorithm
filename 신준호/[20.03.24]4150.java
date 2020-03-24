b0bf73630ddf8464a4fe79e7242ef9c2714ac60c
Ho <wnsgh578@naver.com>
2020-03-24 19:33:32 +0900

    update

---
+++ 신준호/[20.03.24]4150.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = "";
+        String[] tokens;
+
+        //input = br.readLine();
+        //tokens = input.split(" ");
+
+        int n = Integer.parseInt(br.readLine());
+        BigInteger[] fibo = new BigInteger[n + 1];
+        fibo[0] = BigInteger.ZERO;
+        fibo[1] = BigInteger.ONE;
+        for(int i = 2; i <= n; i++) {
+            fibo[i] = fibo[i-1].add(fibo[i-2]);
+        }
+
+        System.out.println(fibo[n]);
+
+    }
+
+}
