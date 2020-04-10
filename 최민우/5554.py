9407c57d403df739218b723604e1d68b68877d66
chaeminu <mass_13@naver.com>
2020-04-11 03:57:06 +0900

    [20.04.11] Bronze V

---
+++ 최민우/5554.py
@@ -0,0 +1,5 @@
+sum = 0
+for i in range(4):
+    sum += int(input())
+print(sum // 60)
+print(sum % 60)
---
+++ 최민우/9654.py
@@ -0,0 +1,7 @@
+print("SHIP NAME      CLASS          DEPLOYMENT IN SERVICE\n"
+      "N2 Bomber      Heavy Fighter  Limited    21\n"
+      "J-Type 327     Light Combat   Unlimited  1\n"
+      "NX Cruiser     Medium Fighter Limited    18\n"
+      "N1 Starfighter Medium Fighter Unlimited  25\n"
+      "Royal Cruiser  Light Combat   Limited    4"
+      "")
