bbfe9c02df9f6ea21fc83a955987e7aa19a97b26
Ho <wnsgh578@naver.com>
2020-04-13 23:13:50 +0900

    update

---
+++ 신준호/[20.04.13]15624.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = "";
+        String[] tokens;
+
+        input = br.readLine();
+        //tokens = input.split(" ");
+
+        BigInteger n = new BigInteger(input);
+        if(n.equals(BigInteger.ZERO))
+            System.out.println("0");
+        else
+            System.out.println(fibo(n)[0][1]);
+    }
+
+    public static long[][] fibo(BigInteger n) {
+        long[][] fiboMat = new long[2][2];
+        fiboMat[0][0] = 1; fiboMat[0][1] = 1; fiboMat[1][0] = 1; fiboMat[1][1] = 0;
+
+        if(n.equals(BigInteger.ONE)) return fiboMat;
+
+        long[][] half = fibo(n.divide(BigInteger.valueOf(2)));
+
+        if(n.mod(BigInteger.valueOf(2)).equals(BigInteger.ONE))
+            return matMul(matMul(half, half), fiboMat);
+        else
+            return matMul(half, half);
+    }
+
+    public static long[][] matMul(long[][] mat1, long[][] mat2) {
+        long[][] ret = new long[2][2];
+        ret[0][0] = ((mat1[0][0] * mat2[0][0]) + (mat1[0][1] * mat2[1][0])) % 1000000007;
+        ret[0][1] = ((mat1[0][0] * mat2[0][1]) + (mat1[0][1] * mat2[1][1])) % 1000000007;
+        ret[1][0] = ((mat1[1][0] * mat2[0][0]) + (mat1[1][1] * mat2[1][0])) % 1000000007;
+        ret[1][1] = ((mat1[1][0] * mat2[0][1]) + (mat1[1][1] * mat2[1][1])) % 1000000007;
+
+        return ret;
+    }
+}
