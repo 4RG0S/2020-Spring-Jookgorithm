a8fbff2c6e4fd1499f9486472bbb7b362644e2ac
chaeminu <mass_13@naver.com>
2020-04-25 03:33:29 +0900

    [20.04.24] Silver V

--- 최민우/11650.py
+++ 최민우/11650.py
@@ -1,6 +1,6 @@
 number_list = []
 for _ in range(int(input())):
     number_list.append(list(map(int, input().split(' '))))
-number_list.sort(key=lambda x: (x[1], x[0]))
+number_list.sort(key=lambda x: (x[0], x[1]))
 for i in number_list:
     print(i[0], i[1])
